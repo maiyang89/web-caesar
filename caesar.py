dfc41c729905be496689b05a480d3a8a298f047b
Mai Yang <Mai.Yang@BlueMoonInd.com>
2017-02-02 14:47:15 -0500

    Fixed Rotate Character Function

--- caesar.py
+++ caesar.py
@@ -1,42 +1,28 @@
+import string
+
 def alphabet_position(letter):
-    alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
-    alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if letter in alphabet_lower:
-        return alphabet_lower.index(letter)
-    if letter in alphabet_upper:
-        return alphabet_upper.index(letter)
+    alphabet = string.ascii_lowercase
+
+    if letter in alphabet:
+        return alphabet.index(letter)
     return letter
 
 def rotate_character(char,rot):
-    alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
-    alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    position = alphabet_position(char)
+    alphabet = string.ascii_uppercase
+    position = alphabet_position(char.lower())
 
-    if rot % 26 != 0 and rot <= 26 and char.isupper():
-        if position + rot > 25:
-            return alphabet_upper[position - 26 + rot]
+    #confirm if character is alphabetic
+    if char.isalpha():
+        #confirm if character is uppercase
+        if char.isupper():
+            #return the rotated character capitalized
+            return alphabet[(position + rot) % len(alphabet)]
         else:
-            return alphabet_upper[position + rot]
-    if rot % 26 != 0 and rot > 26 and char.isupper():
-        if rot - 26 + position > 25:
-            return alphabet_upper[rot - 26 + position - 26]
-        else:
-            return alphabet_upper[rot - 26 + position]
-    if rot % 26 != 0 and rot <= 26 and char.islower():
-        if position + rot > 25:
-            return alphabet_lower[position - 26 + rot]
-        else:
-            return alphabet_lower[position + rot]
-    if rot % 26 != 0 and rot > 26 and char.islower():
-        if rot - 26 + position > 25:
-            return alphabet_lower[rot - 26 + position - 26]
-        else:
-            return alphabet_lower[rot - 26 + position]
-    if char.islower():
-        return alphabet_lower[position]
-    if char.isupper():
-        return alphabet_upper[position]
-    return position
+            #return the rotated character lowercased
+            return string.ascii_lowercase[(position + rot) % len(alphabet)]
+    #return character if not alphabetic
+    else:
+        return char
 
 def encrypt(text,rot):
     newMsg = ""
